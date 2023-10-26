6b681e2ca6dc9f72be8477dfac0bbd02c2e92dff
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2023-10-26 15:01:59 +0530

    largestOf2Numbers

---
+++ largest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter 2 numbers: ";
+	cin>>a>>b;
+	if(a>b)
+	cout<<"a is largest";
+	else if(a<b)
+	cout<<"b is largest";
+	else
+	cout<<"Both are equal";
+}
