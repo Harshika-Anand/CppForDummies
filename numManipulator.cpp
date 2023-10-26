c5b28f06f959fb2f3dbd07ae1084949d0739208c
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2023-10-27 00:18:20 +0530

    numManipulator

---
+++ numManipulator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a hexadecimal number: ";
+	cin>>hex>>n;
+	cout<<"Octal: "<<oct<<n<<" ";
+	cout<<"Decimal: "<<dec<<n;
+}
